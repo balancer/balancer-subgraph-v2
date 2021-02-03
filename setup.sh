72dabaa27a331a3982842c0f3006a85361e22096
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-02 22:02:02 -0800

    use pool-management-v2 submodule on branch that uses docker (pending merge)

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 git submodule add git@github.com:balancer-labs/balancer-core-v2.git core
 cd core && yarn && popd
-git submodule add git@github.com:balancer-labs/balancer.eth.git balancer.eth
-cd balancer.eth && npm install && popd
+
+git submodule add git@github.com:balancer-labs/pool-management-v2.git pool-management-v2
+cd pool-management-v2 && npm install && popd
