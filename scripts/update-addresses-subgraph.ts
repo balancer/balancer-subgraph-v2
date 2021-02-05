b79103deed2f19460bc06cc168610395e1a98709
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-04 21:02:53 -0800

    added kovan subgraph config, modified scripts to keep it updated

--- scripts/update-addresses-subgraph.ts
+++ scripts/update-addresses-subgraph.ts
@@ -11,20 +11,24 @@ function contractAddress(contractName) {
 }
 
 
-const subgraphFilePath = path.resolve(__dirname, '../subgraph.yaml')
+const suffix: string = network == 'dockerParity' ? '' : '.' + network
+
+const subgraphFilePath = path.resolve(__dirname, '../subgraph' + suffix + '.yaml')
+console.log(subgraphFilePath)
 let doc = yaml.load(fs.readFileSync(subgraphFilePath, 'utf8'));
 
-try {
-  doc.dataSources.forEach((ds) => {
+doc.dataSources.forEach((ds) => {
+  try {
     let address = contractAddress(ds.name)
     ds.source.address = address
-  })
-  fs.writeFile(subgraphFilePath, yaml.dump(doc, {lineWidth: 150}), (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-} catch (err) {
-  const msg = 'Cannot find ' + network + ' contract artifacts - have you deployed core contracts to it?'
-  console.log(msg)
-}
+  } catch (err) {
+    const msg = 'Cannot find ' + network + ' contract artifacts for ' + ds.name + ' - missing deployment artifacts?'
+    console.log(msg)
+  }
+})
+
+fs.writeFile(subgraphFilePath, yaml.dump(doc, {lineWidth: 150}), (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
