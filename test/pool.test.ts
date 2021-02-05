c4a879ae1837e0f0d3ff2e69a54d51fa81baed30
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-04 20:04:24 -0800

    added jest and basic pool tests

---
+++ test/pool.test.ts
@@ -0,0 +1,18 @@
+import { runTestCases } from './test-runner';
+import { gql } from '@apollo/client/core'
+
+const testCases = [
+  {
+    id: 'getPools',
+    query: gql`
+            query {
+                pools {
+                    tokens { id }
+                    id
+                }
+            }
+        `
+  },
+]
+
+runTestCases('Pools', testCases);
