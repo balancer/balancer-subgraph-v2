9d42039a414a6d4c3298365567bfb563fd738c3e
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-22 10:09:11 -0800

    create and track stable pools

--- src/mappings/poolFactory.ts
+++ src/mappings/poolFactory.ts
@@ -3,7 +3,7 @@ import {
 } from './constants';
 import { newPoolEntity } from './helpers'
 
-import { BigInt, BigDecimal, Address, Bytes, store } from '@graphprotocol/graph-ts';
+import { BigInt, BigDecimal, Address, Bytes, store, ethereum } from '@graphprotocol/graph-ts';
 import {
   PoolCreated
 } from '../types/WeightedPoolFactory/WeightedPoolFactory';
@@ -11,37 +11,63 @@ import { Balancer, Pool, Swap, TokenPrice, User, UserBalance, PoolTokenizer } fr
 
 // datasource
 import { WeightedPool as WeightedPoolTemplate } from '../types/templates'
-//import { StablePool as StablePoolTemplate } from '../types/templates'
+import { StablePool as StablePoolTemplate } from '../types/templates'
 
 import { WeightedPool } from '../types/templates/WeightedPool/WeightedPool'
+import { StablePool } from '../types/templates/StablePool/StablePool'
 
-export function handleNewPool(event: PoolCreated): void {
-  let vault = Balancer.load('2');
-
-  // if no vault yet, set up blank initial
-  if (vault == null) {
-    vault = new Balancer('2');
-    vault.color = 'Silver';
-    vault.poolCount = 0;
-    vault.finalizedPoolCount = 0;
-    vault.txCount = BigInt.fromI32(0);
-    vault.totalLiquidity = ZERO_BD;
-    vault.totalSwapVolume = ZERO_BD;
-    vault.totalSwapFee = ZERO_BD;
-  }
-
+export function handleNewWeightedPool(event: PoolCreated): void {
   let poolAddress: Address = event.params.pool
-  let poolContract = WeightedPool.bind(poolAddress);
+  let poolContract = WeightedPool.bind(poolAddress)
 
   let poolIdCall = poolContract.try_getPoolId();
   let poolId = poolIdCall.value;
 
+  let swapFeeCall = poolContract.try_getSwapFee();
+  let swapFee = swapFeeCall.value;
+
+  handleNewPool(event, poolId, swapFee)
+  WeightedPoolTemplate.create(poolAddress);
+}
+
+export function handleNewStablePool(event: PoolCreated): void {
+  let poolAddress: Address = event.params.pool
+  let poolContract = StablePool.bind(poolAddress)
+
+  let poolIdCall = poolContract.try_getPoolId();
+  let poolId = poolIdCall.value;
+
+  let swapFeeCall = poolContract.try_getSwapFee();
+  let swapFee = swapFeeCall.value;
+
+  handleNewPool(event, poolId, swapFee)
+  StablePoolTemplate.create(poolAddress)
+}
+
+function findOrInitializeVault(): Balancer {
+  let vault: Balancer | null = Balancer.load('2');
+  if (vault !== null) return vault as Balancer;
+
+  // if no vault yet, set up blank initial
+  vault = new Balancer('2');
+  vault.color = 'Silver';
+  vault.poolCount = 0;
+  vault.finalizedPoolCount = 0;
+  vault.txCount = BigInt.fromI32(0);
+  vault.totalLiquidity = ZERO_BD;
+  vault.totalSwapVolume = ZERO_BD;
+  vault.totalSwapFee = ZERO_BD;
+  return vault as Balancer;
+}
+
+function handleNewPool(event: PoolCreated, poolId: Bytes, swapFee: BigInt): void {
+  let vault = findOrInitializeVault();
+
+  let poolAddress: Address = event.params.pool
+
   let pool = Pool.load(poolId.toHexString())
   if (pool == null) {
     pool = newPoolEntity(poolId.toHexString());
-
-    let swapFeeCall = poolContract.try_getSwapFee();
-    let swapFee = swapFeeCall.value;
 
     pool.swapFee = swapFee.toBigDecimal();
     pool.createTime = event.block.timestamp.toI32();
@@ -49,9 +75,6 @@ export function handleNewPool(event: PoolCreated): void {
     pool.tx = event.transaction.hash;
 
     pool.save();
-
-    // start receiving events
-    WeightedPoolTemplate.create(poolAddress);
   }
 
   vault.poolCount = vault.poolCount + 1;
@@ -63,5 +86,3 @@ export function handleNewPool(event: PoolCreated): void {
   poolTokenizer.holdersCount = BigInt.fromI32(0);
   poolTokenizer.save();
 }
-
-
