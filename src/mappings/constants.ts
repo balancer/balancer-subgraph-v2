8f00f7c9cc067ac81c8a940a69a4c7a64b418510
Greg Taschuk <gregorytaschuk@gmail.com>
2021-03-08 04:19:24 -0800

    added in pricing assets for kovan

--- src/mappings/constants.ts
+++ src/mappings/constants.ts
@@ -1,53 +1,69 @@
 import { BigDecimal, Address, BigInt, Bytes, dataSource, ethereum } from '@graphprotocol/graph-ts';
 
-let network = dataSource.network();
 
 export let ZERO_BD = BigDecimal.fromString('0');
 
 export let ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-export let WETH: Address =
-  Address.fromString(
-    network == 'mainnet'
-    ? '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-    : '0x4CDDb3505Cf09ee0Fa0877061eB654839959B9cd'
-  );
+export class AddressByNetwork {
+  public mainnet: string;
+  public kovan: string;
+  public dev: string;
+}
 
-export let WBTC: Address =
-  Address.fromString(
-    network == 'mainnet'
-    ? '0xTODO'
-    : '0xcD80986f08d776CE41698c47f705CDc99dDBfB0A'
-  );
+let network: string = dataSource.network();
 
-export let USD: Address =
-  Address.fromString(
-    network == 'mainnet'
-    ? '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' // USDC
-    : '0x1528f3fcc26d13f7079325fb78d9442607781c8c'
-  ); // DAI
+let wethAddressByNetwork: AddressByNetwork = {
+  mainnet: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  kovan: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
+  dev: '0x4CDDb3505Cf09ee0Fa0877061eB654839959B9cd',
+};
 
-export let USDC: Address =
-  Address.fromString(
-    network == 'mainnet'
-    ? '0xTODO'
-    : '0x7c0c5AdA758cf764EcD6bAC05b63b2482f90bBB2'
-  )
+let wbtcAddressByNetwork: AddressByNetwork = {
+  mainnet: '0xTODO',
+  kovan: '0x3994596ad2114bc369e3e542abee9bc3d2c071b1',
+  dev: '0xcD80986f08d776CE41698c47f705CDc99dDBfB0A',
+};
 
-export let DAI: Address =
-  Address.fromString(
-    network == 'mainet'
-    ? '0x6B175474E89094C44Da98b954EedeAC495271d0F'
-    : '0x5C0E66606eAbEC1df45E2ADd26C5DF8C0895a397'
-  )
+let usdAddressByNetwork: AddressByNetwork = {
+  mainnet: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
+  kovan: '0xTODO',
+  dev: '0x1528f3fcc26d13f7079325fb78d9442607781c8c',
+};
+let usdcAddressByNetwork: AddressByNetwork = {
+  mainnet: '0xTODO',
+  kovan: '0x2f375e94fc336cdec2dc0ccb5277fe59cbf1cae5',
+  dev: '0x7c0c5AdA758cf764EcD6bAC05b63b2482f90bBB2',
+};
 
-export let BAL: Address =
-  Address.fromString(
-    network == 'mainet'
-    ? '0xba100000625a3754423978a60c9317c58a424e3D'
-    : '0xf702269193081364E355f862f2CFbFCdC5DB738C'
-  )
+let balAddressByNetwork: AddressByNetwork = {
+  mainnet: '0xba100000625a3754423978a60c9317c58a424e3D',
+  kovan: '0x16c6a736b28d92aae496bb30f937826798afc63c',
+  dev: '0xf702269193081364E355f862f2CFbFCdC5DB738C',
+};
 
+let daiAddressByNetwork: AddressByNetwork = {
+  mainnet: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  kovan: '0x1528f3fcc26d13f7079325fb78d9442607781c8c',
+  dev: '0x5C0E66606eAbEC1df45E2ADd26C5DF8C0895a397',
+};
+
+function forNetwork(addressByNetwork: AddressByNetwork, network: string): Address {
+  if (network == 'mainnet') {
+    return Address.fromString(addressByNetwork.mainnet)
+  } else if (network == 'kovan') {
+    return Address.fromString(addressByNetwork.kovan)
+  } else {
+    return Address.fromString(addressByNetwork.dev)
+  }
+}
+
+export let WETH: Address = forNetwork(wethAddressByNetwork, network);
+export let WBTC: Address = forNetwork(wbtcAddressByNetwork, network);
+export let USD: Address = forNetwork(usdAddressByNetwork, network);
+export let USDC: Address = forNetwork(usdcAddressByNetwork, network);
+export let BAL: Address = forNetwork(balAddressByNetwork, network);
+export let DAI: Address = forNetwork(daiAddressByNetwork, network);
 
 export let PRICING_ASSETS: Address[] = [WETH, WBTC, USDC, DAI, BAL];
 export let USD_STABLE_ASSETS: Address[] = [USDC, DAI];
