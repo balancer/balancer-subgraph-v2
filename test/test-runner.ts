eb6a3242f64d451875bf76d720b2fa4484411e49
Greg Taschuk <gregorytaschuk@gmail.com>
2021-04-01 15:31:30 -0700

    fixup lint in test files

--- test/test-runner.ts
+++ test/test-runner.ts
@@ -1,29 +1,22 @@
 import { DocumentNode } from '@apollo/client/core';
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-} from '@apollo/client/core';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core';
 
-const fetch = require("node-fetch");
+import fetch from 'node-fetch';
 
 interface TestCase {
-  id: string
-  query: DocumentNode, 
-  variables?: Object
+  id: string;
+  query: DocumentNode;
+  variables?: any;
 }
 
-const SUBGRAPH_QUERY_ENDPOINT = 'http://127.0.0.1:8000/subgraphs/name/balancer-labs/balancer-v2'
-let cache = new InMemoryCache(); 
+const SUBGRAPH_QUERY_ENDPOINT = 'http://127.0.0.1:8000/subgraphs/name/balancer-labs/balancer-v2';
+let cache = new InMemoryCache();
 
-export const runTestCases = async (
-  groupName: string,
-  testCases: TestCase[],
-) => {
+export const runTestCases = async (groupName: string, testCases: TestCase[]) => {
   const linkOptions = { uri: SUBGRAPH_QUERY_ENDPOINT, fetch };
-  const link = createHttpLink(linkOptions)
-  const cache = new InMemoryCache()
-  let aClient = new ApolloClient({link, cache})
+  const link = createHttpLink(linkOptions);
+  const cache = new InMemoryCache();
+  let aClient = new ApolloClient({ link, cache });
 
   describe(`${groupName} resolvers`, () => {
     for (let testCase of testCases) {
@@ -34,6 +27,6 @@ export const runTestCases = async (
         });
         expect(res).toMatchSnapshot();
       });
-    };
+    }
   });
 };
