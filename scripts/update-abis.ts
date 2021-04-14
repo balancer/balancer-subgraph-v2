68702801bf63564cd38e0c5948e53854176782e9
Greg Taschuk <gregorytaschuk@gmail.com>
2021-04-14 14:09:29 -0700

    added script to update the abis from core artifacts

---
+++ scripts/update-abis.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as glob from 'glob';
+
+const coreArtifactPath = __dirname + '/../../core/artifacts';
+const outputPath = __dirname + '/../abis';
+
+const abiJSONfilenames = [
+  'Vault',
+  'WeightedPool',
+  'StablePool',
+  'WeightedPoolFactory',
+  'StablePoolFactory',
+  'BalancerPoolToken',
+  'BasePoolFactory',
+  'ERC20',
+  'BalancerHelpers',
+].map((a) => a + '.json');
+
+glob(coreArtifactPath + '/**/!(*dbg).json', {}, (err, files) => {
+  if (err) {
+    console.error(err);
+  }
+  for (const filename of files) {
+    const split = filename.split('/');
+    const contractName = split[split.length - 1];
+    if (abiJSONfilenames.includes(contractName)) {
+      console.log(contractName);
+
+      const abi = JSON.parse(fs.readFileSync(path.resolve(filename), 'utf8')).abi;
+
+      fs.writeFileSync(outputPath + '/' + contractName, JSON.stringify(abi, null, 2));
+    }
+  }
+});
