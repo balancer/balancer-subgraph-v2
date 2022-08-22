967916a3f8baa211d56e917b75229d42b45f1c07
Fábio Mendes <ms.fabiomendes@gmail.com>
2022-08-22 15:32:04 -0300

    improve file generated

--- scripts/generate-assets.ts
+++ scripts/generate-assets.ts
@@ -9,6 +9,7 @@ const network = process.argv[2] || 'mainnet';
 const generateAssets = async (): Promise<void> => {
   const networksFilePath = path.resolve(__dirname, `../assets/${network}.json`);
   const config = yaml.load(await fs.readFile(networksFilePath, { encoding: 'utf-8' }));
+  config['network'] = network;
 
   const template = fs.readFileSync('assets.handlebars').toString();
   fs.writeFileSync('src/mappings/helpers/assets.ts', Handlebars.compile(template)(config));
