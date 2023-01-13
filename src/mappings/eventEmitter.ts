82ffd3ea65d3b432a506454e5042e7fd406e71a0
Markus Buhatem Koch <markusbkoch@gmail.com>
2023-01-13 16:00:52 -0300

    refactor mapping for bytes32 identifier

--- src/mappings/eventEmitter.ts
+++ src/mappings/eventEmitter.ts
@@ -1,26 +1,24 @@
+import { Bytes, crypto } from '@graphprotocol/graph-ts';
 import { LogArgument } from '../types/EventEmitter/EventEmitter';
 import { Pool } from '../types/schema';
 
 export function handleLogArgument(event: LogArgument): void {
-  const identifier = event.params.identifier;
+  const identifier = event.params.identifier.toHexString();
 
-  if (identifier == 'setSwapEnabledTrue') {
-    const poolId = event.params.message.toHexString();
-    const pool = Pool.load(poolId);
-
-    if (!pool) return;
-
-    pool.swapEnabled = true;
-    pool.save();
+  if (identifier == '0xe84220e19c54dd2a96deb4cb59ea58e10e36ae2e5c0887f3af0a1ac8e04b0e29') { //setSwapEnabled
+    setSwapEnabled(event);
   }
+}
 
-  if (identifier == 'setSwapEnabledFalse') {
-    const poolId = event.params.message.toHexString();
-    const pool = Pool.load(poolId);
+function setSwapEnabled(event: LogArgument): void {
+  const poolId = event.params.message.toHexString();
+  const pool = Pool.load(poolId);
+  if (!pool) return;
 
-    if (!pool) return;
-
+  if (event.params.value == 0) {
     pool.swapEnabled = false;
-    pool.save();
+  } else {
+    pool.swapEnabled = true;
   }
+  pool.save();
 }
