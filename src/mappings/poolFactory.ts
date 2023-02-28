434aaa307384d47c844a86ad4f500bcf23cb72ee
baileyspraggins <baileyspraggins@me.com>
2023-02-27 20:17:16 -0600

    merge with staging-dev

--- src/mappings/poolFactory.ts
+++ src/mappings/poolFactory.ts
@@ -20,7 +20,7 @@ import { updatePoolWeights } from './helpers/weighted';
 import { BigInt, Address, Bytes, BigDecimal, ethereum } from '@graphprotocol/graph-ts';
 import { PoolCreated } from '../types/WeightedPoolFactory/WeightedPoolFactory';
 import { AaveLinearPoolCreated } from '../types/AaveLinearPoolV3Factory/AaveLinearPoolV3Factory';
-import { ProtocolIdRegistered, ProtocolIdRenamed } from '../types/ProtocolIdRegistry/ProtocolIdRegistry';
+import { ProtocolIdRegistered } from '../types/ProtocolIdRegistry/ProtocolIdRegistry';
 import { Balancer, Pool, PoolContract, ProtocolIdData } from '../types/schema';
 
 // datasource
