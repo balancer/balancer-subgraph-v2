3d0ba280eeb3b9dcaf0d8d57455dcb98f5294a56
Fábio Mendes <ms.fabiomendes@gmail.com>
2023-08-30 13:56:19 -0300

    prevent pricing bpts on join exit swaps

--- src/mappings/vault.ts
+++ src/mappings/vault.ts
@@ -572,7 +572,8 @@ export function handleSwapEvent(event: SwapEvent): void {
 
   let swapId = transactionHash.toHexString().concat(logIndex.toString());
 
-  if (poolAddress == tokenInAddress || poolAddress == tokenOutAddress) {
+  const isJoinExitSwap = poolAddress == tokenInAddress || poolAddress == tokenOutAddress;
+  if (isJoinExitSwap) {
     if (isComposableStablePool(pool)) {
       let tokenAddresses = pool.tokensList;
       let balances: BigInt[] = [];
@@ -668,7 +669,12 @@ export function handleSwapEvent(event: SwapEvent): void {
   let blockNumber = event.block.number;
   let tokenInWeight = poolTokenIn.weight;
   let tokenOutWeight = poolTokenOut.weight;
-  if (isPricingAsset(tokenInAddress) && pool.totalLiquidity.gt(MIN_POOL_LIQUIDITY) && valueUSD.gt(MIN_SWAP_VALUE_USD)) {
+  if (
+    !isJoinExitSwap &&
+    isPricingAsset(tokenInAddress) &&
+    pool.totalLiquidity.gt(MIN_POOL_LIQUIDITY) &&
+    valueUSD.gt(MIN_SWAP_VALUE_USD)
+  ) {
     let tokenPriceId = getTokenPriceId(poolId.toHex(), tokenOutAddress, tokenInAddress, blockNumber);
     let tokenPrice = new TokenPrice(tokenPriceId);
     //tokenPrice.poolTokenId = getPoolTokenId(poolId, tokenOutAddress);
@@ -693,6 +699,7 @@ export function handleSwapEvent(event: SwapEvent): void {
     updateLatestPrice(tokenPrice, event.block.timestamp);
   }
   if (
+    !isJoinExitSwap &&
     isPricingAsset(tokenOutAddress) &&
     pool.totalLiquidity.gt(MIN_POOL_LIQUIDITY) &&
     valueUSD.gt(MIN_SWAP_VALUE_USD)
