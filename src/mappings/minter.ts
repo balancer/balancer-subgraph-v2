b0289543839be49cac4cde46bb263b4ddcfac861
Kaliman <akalimancevic@gmail.com>
2024-01-17 13:26:58 +0100

    feat: minter contract subgraph added

---
+++ src/mappings/minter.ts
@@ -0,0 +1,46 @@
+import { log, BigInt } from '@graphprotocol/graph-ts';
+import { Minter, TokenMint } from '../types/schema';
+import { JellyMinted, MintingStarted } from '../types/templates/Minter/Minter';
+
+export function handleJellyMinted(event: JellyMinted): void {
+  let minter = Minter.load(event.address.toHexString()) as Minter;
+  if (minter == null) {
+    log.error('Minter {} does not exist', [event.address.toHexString()]);
+    return;
+  }
+  let tokenMintLpRewards = new TokenMint(event.transaction.hash.toHexString() + '-LP');
+  tokenMintLpRewards.minter = minter.id;
+  tokenMintLpRewards.amount = event.params.mintedAmount.div(BigInt.fromString('2'));
+  tokenMintLpRewards.timestamp = event.block.timestamp;
+  tokenMintLpRewards.mintInitiator = event.params.sender;
+  tokenMintLpRewards.benefactor = event.params.lpRewardsContract;
+  tokenMintLpRewards.save();
+
+  let tokenMintStakingRewards = new TokenMint(event.transaction.hash.toHexString() + '-Staking');
+  tokenMintStakingRewards.minter = minter.id;
+  tokenMintStakingRewards.amount = event.params.mintedAmount.div(BigInt.fromString('2'));
+  tokenMintStakingRewards.timestamp = event.block.timestamp;
+  tokenMintStakingRewards.mintInitiator = event.params.sender;
+  tokenMintStakingRewards.benefactor = event.params.stakingRewardsContract;
+  tokenMintStakingRewards.save();
+
+  minter.lastMintedTimestamp = event.params.mintingPeriod;
+  if (minter.benfactors.indexOf(event.params.lpRewardsContract) == -1) {
+    minter.benfactors.push(event.params.lpRewardsContract);
+  }
+  if (minter.benfactors.indexOf(event.params.stakingRewardsContract) == -1) {
+    minter.benfactors.push(event.params.stakingRewardsContract);
+  }
+  minter.lastMintedAmount = event.params.mintedAmount;
+  minter.mintingPeriod = event.params.mintingPeriod;
+  minter.save();
+}
+
+export function handleMintingStarted(event: MintingStarted): void {
+  let newMinter = new Minter(event.address.toHexString());
+  newMinter.mintingPeriod = event.params.startTimestamp;
+  newMinter.lastMintedTimestamp = event.params.startTimestamp;
+  newMinter.lastMintedAmount = BigInt.fromI32(0);
+  newMinter.benfactors = [];
+  newMinter.save();
+}
