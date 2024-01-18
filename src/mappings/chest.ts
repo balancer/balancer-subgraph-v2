51840a3fccb67e85a45697bce3d5dc571bcf30a1
Nikola Panajotov <11493864+npanajotov@users.noreply.github.com>
2024-01-18 17:20:45 +0100

    Add handlers for chest, unstake and increase stake

--- src/mappings/chest.ts
+++ src/mappings/chest.ts
@@ -1,233 +1,70 @@
+import { Bytes, log } from '@graphprotocol/graph-ts';
+import { Chest, IncreaseStake, Unstake } from '../types/schema';
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  FeeWithdrawn as FeeWithdrawnEvent,
   IncreaseStake as IncreaseStakeEvent,
-  NewVestingPosition as NewVestingPositionEvent,
-  OwnershipTransferCanceled as OwnershipTransferCanceledEvent,
-  OwnershipTransferRequested as OwnershipTransferRequestedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  SetFee as SetFeeEvent,
-  SetMaxBooster as SetMaxBoosterEvent,
-  Staked as StakedEvent,
-  Transfer as TransferEvent,
+  Staked as ChestEvent,
   Unstake as UnstakeEvent,
-} from "../generated/Chest/Chest"
-import {
-  Approval,
-  ApprovalForAll,
-  FeeWithdrawn,
-  IncreaseStake,
-  NewVestingPosition,
-  OwnershipTransferCanceled,
-  OwnershipTransferRequested,
-  OwnershipTransferred,
-  SetFee,
-  SetMaxBooster,
-  Staked,
-  Transfer,
-  Unstake,
-} from "../generated/schema"
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleFeeWithdrawn(event: FeeWithdrawnEvent): void {
-  let entity = new FeeWithdrawn(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.beneficiary = event.params.beneficiary
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+} from '../types/templates/Chest/Chest';
 
 export function handleIncreaseStake(event: IncreaseStakeEvent): void {
-  let entity = new IncreaseStake(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.tokenId = event.params.tokenId
-  entity.totalStaked = event.params.totalStaked
-  entity.freezedUntil = event.params.freezedUntil
-  entity.booster = event.params.booster
+  let entity = new IncreaseStake(Bytes.fromBigInt(event.params.tokenId));
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (entity == null) {
+    log.error('Increase stake entity does not exist', [event.address.toHexString()]);
+    return;
+  }
 
-  entity.save()
+  entity.tokenId = event.params.tokenId;
+  entity.totalStaked = entity.totalStaked.plus(event.params.totalStaked);
+  entity.freezedUntil = entity.freezedUntil.plus(event.params.freezedUntil);
+  entity.booster = event.params.booster;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
-export function handleNewVestingPosition(event: NewVestingPositionEvent): void {
-  let entity = new NewVestingPosition(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.position_totalVestedAmount = event.params.position.totalVestedAmount
-  entity.position_releasedAmount = event.params.position.releasedAmount
-  entity.position_cliffTimestamp = event.params.position.cliffTimestamp
-  entity.position_vestingDuration = event.params.position.vestingDuration
-  entity.position_freezingPeriod = event.params.position.freezingPeriod
-  entity.position_booster = event.params.position.booster
-  entity.position_nerfParameter = event.params.position.nerfParameter
-  entity.index = event.params.index
+export function handleStaked(event: ChestEvent): void {
+  let entity: Chest = new Chest(event.address);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.user = event.params.user;
+  entity.tokenId = event.params.tokenId;
+  entity.amount = event.params.amount;
+  entity.freezedUntil = event.params.freezedUntil;
+  entity.vestingDuration = event.params.vestingDuration;
+  entity.booster = event.params.booster;
+  entity.nerfParameter = event.params.nerfParameter;
 
-  entity.save()
-}
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-export function handleOwnershipTransferCanceled(
-  event: OwnershipTransferCanceledEvent,
-): void {
-  let entity = new OwnershipTransferCanceled(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferRequested(
-  event: OwnershipTransferRequestedEvent,
-): void {
-  let entity = new OwnershipTransferRequested(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent,
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetFee(event: SetFeeEvent): void {
-  let entity = new SetFee(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.fee = event.params.fee
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetMaxBooster(event: SetMaxBoosterEvent): void {
-  let entity = new SetMaxBooster(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.maxBooster = event.params.maxBooster
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleStaked(event: StakedEvent): void {
-  let entity = new Staked(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.user = event.params.user
-  entity.tokenId = event.params.tokenId
-  entity.amount = event.params.amount
-  entity.freezedUntil = event.params.freezedUntil
-  entity.vestingDuration = event.params.vestingDuration
-  entity.booster = event.params.booster
-  entity.nerfParameter = event.params.nerfParameter
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
 
 export function handleUnstake(event: UnstakeEvent): void {
-  let entity = new Unstake(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.tokenId = event.params.tokenId
-  entity.amount = event.params.amount
-  entity.totalStaked = event.params.totalStaked
-  entity.booster = event.params.booster
+  let entity = Unstake.load(Bytes.fromBigInt(event.params.tokenId));
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (entity == null) {
+    log.error('Unstake entity does not exist', [event.address.toString()]);
+    return;
+  }
 
-  entity.save()
+  if (event.params.amount > entity.amount){
+    log.error('Requested amount cannot be bigger that current amount!', [event.address.toString()]);
+    return;
+  }
+
+  entity.tokenId = event.params.tokenId;
+  entity.amount = event.params.amount;
+  entity.totalStaked = event.params.totalStaked;
+  entity.booster = event.params.booster;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
