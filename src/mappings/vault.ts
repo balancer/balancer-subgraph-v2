8f71e99f2acb25fd8a0871632ab949b13a94323f
gmbronco <83549293+gmbronco@users.noreply.github.com>
2026-02-25 11:26:51 +0100

    paidProtocolFees restored

--- src/mappings/vault.ts
+++ src/mappings/vault.ts
@@ -105,6 +105,9 @@ function handlePoolJoined(event: PoolBalanceChanged): void {
     let amountIn = amounts[i].minus(protocolFeeAmounts[i]);
     let tokenAmountIn = tokenToDecimal(amountIn, poolToken.decimals);
     let newBalance = poolToken.balance.plus(tokenAmountIn);
+    let paidProtocolFees = poolToken.paidProtocolFees ? poolToken.paidProtocolFees : ZERO_BD;
+    let protocolFeeAmount = tokenToDecimal(protocolFeeAmounts[i], poolToken.decimals);
+    poolToken.paidProtocolFees = paidProtocolFees.plus(protocolFeeAmount);
     poolToken.balance = newBalance;
     poolToken.save();
 
@@ -205,7 +208,9 @@ function handlePoolExited(event: PoolBalanceChanged): void {
     let amountOut = amounts[i].minus(protocolFeeAmounts[i]).neg();
     let tokenAmountOut = tokenToDecimal(amountOut, poolToken.decimals);
     let newBalance = poolToken.balance.minus(tokenAmountOut);
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    let paidProtocolFees = poolToken.paidProtocolFees ? poolToken.paidProtocolFees : ZERO_BD;
+    let protocolFeeAmount = tokenToDecimal(protocolFeeAmounts[i], poolToken.decimals);
+    poolToken.paidProtocolFees = paidProtocolFees.plus(protocolFeeAmount);
     poolToken.balance = newBalance;
     poolToken.save();
 
